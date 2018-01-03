820f4229ff6aa51c7c10ba5099ddc2c1577531d7
Your Name <you@example.com>
2018-01-03 18:46:13 -0500

    Cambios antes del modulo 3

--- datacamp/Intermediate Python for Data Science/pandas/intento2.py
+++ datacamp/Intermediate Python for Data Science/pandas/intento2.py
@@ -1,9 +1,10 @@
 import pandas as pd 
 datageneral = pd.read_csv("./gapminder.csv", index_col=0)
 datosx = datageneral[["life_exp"]]
+datosxround =round(datosx,0)
 datosy = datageneral[["gdp_cap"]]
-paises = datageneral[["country"]]
-#print (datosx)
+paises = datageneral[["cont"]]
+print (datosxround)
 #print (datosy)
 
 import numpy as np
@@ -12,10 +13,10 @@ datosxsize2 = datosxsize *6
 
 import matplotlib.pyplot as plt 
 color = ["red","blue","green","yellow","black"]
-grafi = plt.scatter(datosx,paises, s= datosxsize2,alpha=0.7, c=color)
+grafi = plt.scatter(datosxround,paises, alpha=0.7, c=color)
 plt.xlabel("ciclo de vida")
 plt.ylabel("tasa de dispersion")
 
-plt.xticks([10,20,30,40,50,60,70,80,90,100])
-plt.yticks([10,20,30,40,50,60,70,80,90,100])
+plt.xticks([0,10,20,30,40,50,60,70,80],[40,50,60,70,80])
 plt.show()
+#dpinilla@hdv.com
