7a5b6cfc9749272110b43ed9ca6f75da5f781f1f
Your Name <you@example.com>
2018-01-24 13:35:24 -0500

    pivot

--- datacamp/Introduction to Data Visualization with Python/Working with 2D arrays/linspace meshgrid ejercicio4.py
+++ datacamp/Introduction to Data Visualization with Python/Working with 2D arrays/linspace meshgrid ejercicio4.py
@@ -6,9 +6,9 @@ A = np.array([[1, 0, -1], [2, 0, 1], [1, 1, 1]])
 
 x,y = np.meshgrid(A[0],A[1])
 
-z =A[2] + A[1]
+z =x**2/25 + y**2/4
 
 plt.set_cmap("nipy_spectral") 
-plt.pcolor()
+plt.pcolor(z)
 plt.colorbar()
 plt.show()
